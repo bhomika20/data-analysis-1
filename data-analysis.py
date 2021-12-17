3aceee07e3bf0e6e1242df609d189781e96aaa74
bhomika20 <96047599+bhomika20@users.noreply.github.com>
2021-12-17 12:20:28 +0530

    Update data-analysis.py

--- data-analysis.py
+++ data-analysis.py
@@ -7,7 +7,21 @@ df.isnull().sum()
 
 drop_col = df.isnull().sum()[df.isnull().sum()>35/100 * df.shape[0]]
 drop_col
+drop_col.index
 
+df.drop(drop_col.index ,axis=1 ,inplace=True)
+df.isnull().sum()
+
+df.fillna(df.mean(), inplace=True)
+df.isnull().sum()
+
+df['Embarked'].describe()
+
+df['Embarked'].fillna('S', inplace= True)
+
+df.isnull().sum()
+
+df.corr()
 
 
 
